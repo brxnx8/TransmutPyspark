128cf9cc14af0318a43352191eed8213e94d6af2
Eduardo Barros <eduardibarros@gmail.com>
2026-03-24 20:35:40 -0300

    organizando dependencias e versoes

---
+++ transmut-pyspark/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Preparando o ambiente Linux para o PySpark..."
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install openjdk-17-jdk python3-venv -y
+
+echo "Ambiente do sistema operacional pronto!"
+echo "Agora, certifique-se de ter o Poetry instalado e rode 'poetry install'."
